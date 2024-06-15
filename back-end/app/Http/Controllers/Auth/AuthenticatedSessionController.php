94cd37e770bb8a995937906efe8fba06b5664a60
HamedAhmedHamed <hamedahmedgamed@gmail.com>
2024-06-15 10:48:20 +0300

    login

--- back-end/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ back-end/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
@@ -19,13 +20,27 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return response()->noContent();
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials))
+        {
+            $user = Auth::user();
+            // $token = $user->createToken('api-token');
+
+            return response()->json([
+                'user' => $user,
+                // 'token' => $token
+            ]);
+        }
+        else
+        {
+            return response()->noContent();
+        }
     }
 
     /**
      * Destroy an authenticated session.
      */
-    public function destroy(Request $request): Response
+    public function destroy(Request $request): JsonResponse
     {
         Auth::guard('web')->logout();
 
@@ -33,6 +48,8 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerateToken();
 
-        return response()->noContent();
+        return response()->json([
+            'message' => 'user logged out'
+        ]);
     }
 }
